3e990be628d97406b5a99f276a09be79e1fa297b
Honoga-Sensei <120830578+Honoga-Sensei@users.noreply.github.com>
2024-05-09 09:38:38 +0200

    welcome.sh

--- foobar.sh
+++ foobar.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-echo "hello world"
+read -p "Enter name" name
+
+read -p "Continue? (Y/N)" confirm && [$confirm == [Yy] || $confirm == [Yy][Ee][Ss]] || exit 1 
+
+echo "Welcome $name"
+
