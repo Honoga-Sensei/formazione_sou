4afeebce300bf30a0389160b4de3937648ad802f
Honoga-Sensei <120830578+Honoga-Sensei@users.noreply.github.com>
2024-05-09 17:41:23 +0200

    Update ex.sh
    pulizia codice

--- ex.sh
+++ ex.sh
@@ -1,39 +1,28 @@
 #! /bin/bash
 
 #check login dir existance or create it
-
 if [ ! -f /Users/$USER/Desktop/login ]; then
 
 	mkdir /Users/$USER/Desktop/login
 fi 
 
 #check username.txt existance or create it
-
 if [ ! -f /Users/$USER/Desktop/login/username.txt ]; then
 
 	touch /Users/$USER/Desktop/login/username.txt
-
-#check password.txt existance or create it
-
 fi
 
+#check password.txt existance or create it
 if [ ! -f /Users/$USER/Desktop/login/password.txt ]; then
 	
 	touch /Users/$USER/Desktop/login/password.txt	 
-
 fi
 
 #ask if user already has account
-
 read -p "Do you have an account?(Y/N)  " confirmation
 
-i=$((10))
-
-#check for credential on username.txt and password.txt, or create new ones
-
+#check existing account
 if [ $confirmation == "y" ] || [ $confirmation == "Y" ]; then
-
-	i=$((1))
 	
 	for i in {1..3} 
 	do
@@ -41,6 +30,7 @@ if [ $confirmation == "y" ] || [ $confirmation == "Y" ]; then
 		read -p "username: " username
 		read -p "password: " password
 
+#check credential inside txt file
                 if [ $(grep -c "$username" /Users/$USER/Desktop/login/username.txt) == 1 ] && [ $(grep -c "$password" /Users/$USER/Desktop/login/password.txt) == 1 ]; then
 
 				echo "Welcome back $username"
@@ -55,17 +45,28 @@ if [ $confirmation == "y" ] || [ $confirmation == "Y" ]; then
 
 	done
 
-elif [ $confirmation == "n" ] || [ $confirmation == "N" ]; then 
+#create new account
+elif [ $confirmation == "n" ] || [ $confirmation == "N" ]; then
 
-	read -p "username for new account:  " newuser
-	read -p "password for new account:  " newpass
-	
-	echo $newuser >> /Users/$USER/Desktop/login/username.txt
- 	echo $newpass >> /Users/$USER/Desktop/login/password.txt
+        read -p "username for new account:  " newuser
+
+#if user already exist ask for a new one
+        while [ $(grep -c "$newuser" /Users/$USER/Desktop/login/username.txt) == 1 ]; 
+        do      
+
+                read -p "Username already taken, try again:  " newuser
+
+        done    
+
+        read -p "password for new account:  " newpass
+
+        echo $newuser >> /Users/$USER/Desktop/login/username.txt
+        echo $newpass >> /Users/$USER/Desktop/login/password.txt
 
 else
 
-	echo "Invalid character"
+        echo "Invalid character"
 
 fi
+
  
