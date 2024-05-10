ce01a095e602e3f95284e7db76d069d609e82e2d
Honoga-Sensei <120830578+Honoga-Sensei@users.noreply.github.com>
2024-05-10 11:00:33 +0200

    Update login _0.2.sh

--- login _0.2.sh
+++ login _0.2.sh
@@ -7,56 +7,64 @@
 [ -f /Users/$USER/Desktop/login/username.txt ] || touch /Users/$USER/Desktop/login/username.txt
 
 #check password.txt existance or create it
-[ -f /Users/$USER/Desktop/login/password.txt ] || touch /Users/$USER/Desktop/login/password.txt	 
-
-
+[ -f /Users/$USER/Desktop/login/password.txt ] || touch /Users/$USER/Desktop/login/password.txt
 
 #ask if user already has account
-
 read -p "Do you have an account?(Y/N)  " confirmation
 
-#check for credential on username.txt and password.txt, or create new ones
+#create new account if no
+if [[ "$confirmation" =~ [Nn] ]]; then
 
-if [[ "$confirmation" =~ [Yy] ]]; then	
-	for i in {1..3} 
-	do
+        read -p "username for new account:  " newuser
 
-		read -p "username: " username
-		read -p "password: " password
+#if username already exist ask for a new one
+        while grep "$newuser" /Users/$USER/Desktop/login/username.txt>/dev/null
+        do
 
-		if grep "$username" /Users/$USER/Desktop/login/username.txt>/dev/null && grep "$password" /Users/$USER/Desktop/login/password.txt>/dev/null; then
+                read -p "Username already taken, try again:  " newuser
 
-				echo "Welcome back $username"
-				break
-			
-		else	
-			
-			k=$((3-i))
-			echo "Wrong credential, $k attemps remaining"
+        done
 
-		fi
+        read -p "password for new account:  " newpass
+	
+#save new username and password in file then ask for a new login
+        echo $newuser >> /Users/$USER/Desktop/login/username.txt
+        echo $newpass >> /Users/$USER/Desktop/login/password.txt
 
-	done
+        read -p "Would you like to login now?(Y/N)  " confirmation
+fi
+
+#start login process
+if [[ "$confirmation" =~ [Yy] ]]; then
+
+        for i in {1..3}
+        do
+
+                read -p "username: " username
+                read -p "password: " password
+
+#check credential inside txt file
+                if grep "$username" /Users/$USER/Desktop/login/username.txt>/dev/null && grep "$password" /Users/$USER/Desktop/login/password.txt>/dev/null; then
+
+			echo "Welcome back $username"
+                                break
+				
+                else
+
+                        k=$((3-i))
+                        echo "Wrong credential, $k attemps remaining"
+
+                fi
+
+        done
 
 elif [[ "$confirmation" =~ [Nn] ]]; then
-	
-	read -p "username for new account:  " newuser
 
-	while grep "$newuser" /Users/$USER/Desktop/login/username.txt>/dev/null
-	do
-	
-		read -p "Username already taken, try again:  " newuser
-
-		done
-
-	read -p "password for new account:  " newpass
-	
-	echo $newuser >> /Users/$USER/Desktop/login/username.txt
- 	echo $newpass >> /Users/$USER/Desktop/login/password.txt
+        echo "Good bye"
 
 else
 
-	echo "Invalid character"
-
+        echo "Invalid character"
+	
 fi
- 
+
