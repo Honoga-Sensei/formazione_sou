c5cd6ba971c97f7c3e94882ea3986018012999b0
Honoga-Sensei <120830578+Honoga-Sensei@users.noreply.github.com>
2024-05-29 15:46:33 +0200

    Update migration.sh

--- PingPong/migration.sh
+++ PingPong/migration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Set the container so it will run untill closed via shell command
+# Set the container so it will run until closed via shell command
 while true; do  
   sleep infinity
 done
